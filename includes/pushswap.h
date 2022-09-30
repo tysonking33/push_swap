01b001b485ff4ed8b397c20bc5dce59c94e8ac92
Tyson Tang <90588335+tysonking33@users.noreply.github.com>
2022-09-30 18:06:02 +0930

    Add files via upload

---
+++ includes/pushswap.h
@@ -0,0 +1,42 @@
+#ifndef PUSHSWAP_H
+#define PUSHSWAP_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct s_arrays
+{
+	int	*a;
+	int	*b;
+	int	a_size;
+	int	b_size;
+	int step_count;
+	int	*bubble_sort_arr;
+	int	*b_moves;
+	int *a_and_b_moves;
+}			t_arrays;
+
+void	pa(t_arrays *array);
+void	pb(t_arrays *array);
+int		main(int argc, char **argv);
+void	rotatea(t_arrays *array);
+void	error(char *s);
+int		ft_atoi(const char *str);
+void	array_init(t_arrays *array, int argc, char **argv);
+void	print_array(t_arrays *array, char *move_type);
+void    ft_ibzero(t_arrays *array);
+void    rotateb(t_arrays *array);
+void    revrotatea(t_arrays *array);
+void    revrotateb(t_arrays *array);
+void	swapa(t_arrays *array);
+void	swapb(t_arrays *array);
+void	sim_swap(t_arrays *array);
+void    small_med_large(t_arrays *array);
+int		*bubble_sort(t_arrays *array);
+void    large_sort(t_arrays *array);
+int		check_sorted(t_arrays *array);
+void    find_min_moves_arr(t_arrays *array);
+int find_min_moves(t_arrays *array);
+void five_sort(t_arrays * array);
+#endif
---
+++ sources/three_sort.c
@@ -0,0 +1,77 @@
+#include "../includes/pushswap.h"
+
+
+void five_sort(t_arrays * array)
+{
+	pb(array);
+	pb(array);
+	small_med_large(array);
+	while (array->b_size >= 0)
+	{
+		if (array->b[array->b_size] <= array->a[array->a_size])
+		{
+			printf("\n\nmethod 1\n");
+			pa(array);
+		}
+		else if (array->b[array->b_size] <= array->a[array->a_size - 1])
+		{
+			printf("\n\nmethod 2\n");
+			pa(array);
+			swapa(array);
+		}
+		else if (array->b[array->b_size] <= array->a[array->a_size - 2])
+		{
+			if (array->a_size == 2)
+			{
+				printf("\n\nmethod 3\n");
+				revrotatea(array);
+				pa(array);
+				rotatea(array);
+				rotatea(array);
+			}
+			else if (array->a_size == 3)
+			{
+				printf("\n\nmethod 4\n");
+				revrotatea(array);
+				revrotatea(array);
+				pa(array);
+				//rotatea(array);
+				//rotatea(array);
+				//rotatea(array);
+				revrotatea(array);
+				revrotatea(array);
+			}
+
+		}
+		else if (array->b[array->b_size] <= array->a[array->a_size - 3])
+		{
+			printf("\n\nmethod 6\n");
+			revrotatea(array);
+			pa(array);
+			rotatea(array);
+			rotatea(array);
+			//revrotatea(array);
+		}
+		else
+		{
+			printf("\n\nmethod 7\n");
+			pa(array);
+			rotatea(array);
+		}
+	}
+}
+
+
+void	small_med_large(t_arrays *array)
+{
+	if ((array->a[2] > array->a[1]) && (array->a[2] > array->a[0]))
+		rotatea(array);
+	else if ((array->a[1] > array->a[2]) && (array->a[1] > array->a[0]))
+		revrotatea(array);
+	else if ((array->a[1] > array->a[2]) && (array->a[1] < array->a[0]))
+		swapa(array);
+	else if ((array->a[2] > array->a[1]) && (array->a[1] < array->a[0]) && (array->a[2] < array->a[0]))
+		swapa(array);
+	if (array->a[2] > array->a[1])
+		swapa(array);
+}
