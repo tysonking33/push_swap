4372e233d7b725e50bf46d5da9715e9a39cb414b
Tyson Tang <90588335+tysonking33@users.noreply.github.com>
2022-12-01 05:30:59 +0000

    group 2 and 3 are out of position

--- sources/large_sort2.c
+++ sources/large_sort2.c
@@ -78,7 +78,6 @@ void large_sort2(t_arrays *array)
 	array->group_info.top_sort = array->bubble_sort_arr[array->pos.a5_start - 1];
 	array->group_info.bottom_sort = array->bubble_sort_arr[array->pos.a5_end];
 	printf("array->bottom_sort: %d\n", array->group_info.bottom_sort);
-
 	printf("array->top_sort: %d, array->bottom_sort: %d\n", array->group_info.top_sort, array->group_info.bottom_sort);
 	sort(array, array->pos.a6_start, array->pos.a6_end, array->group_info.group_size);
 
@@ -92,16 +91,18 @@ void large_sort2(t_arrays *array)
 	
 	array->group_info.bottom_sort = array->bubble_sort_arr[array->pos.a7_end];
 	array->group_info.top_sort = array->bubble_sort_arr[array->pos.a4_start - 1];
+	array->group_info.ra_toggle = 0;
 	sort(array, array->pos.a3_start, array->pos.a3_end, array->group_info.group_size);
+	
 	if (array->group_info.rem_size != 0)
 	{
 		array->group_info.ra_toggle = 1;
 		sort(array, array->pos.a8_start, array->pos.a8_end, array->group_info.rem_size);
 	}
+	
 	array->group_info.ra_toggle = 0;
 	sort(array, array->pos.a2_start, array->pos.a2_end, array->group_info.group_size);
 	array->group_info.ra_toggle = 0;
-
 	sort(array, array->pos.a1_start, array->pos.a1_end, array->group_info.group_size);
 
 }
